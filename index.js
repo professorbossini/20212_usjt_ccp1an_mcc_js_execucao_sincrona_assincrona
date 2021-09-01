170a6301f6a4e413196b0087005e1483dad8017a
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-31 21:51:55 -0300

    Encerrada a apostila 03. Solucao do exercicio feita pelo Gustavo Pedro

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-function fatorial (n){
-    if (n < 0) return Promise.reject ("Valor não pode ser negativo")
-    let res = 1
-    for (let i = 2; i <= n; i++)
-        res *= i
-    return Promise.resolve(res)
-}
+const axios = require('axios')
+// function fatorial (n){
+//     if (n < 0) return Promise.reject ("Valor não pode ser negativo")
+//     let res = 1
+//     for (let i = 2; i <= n; i++)
+//         res *= i
+//     return Promise.resolve(res)
+// }
 
-function chamaUsandoThenCatch(){
-    fatorial(5)
-    .then (res => console.log ("Res: " + res))
-    .catch (erro => console.log ("Erro: " + erro))
+// function chamaUsandoThenCatch(){
+//     fatorial(5)
+//     .then (res => console.log ("Res: " + res))
+//     .catch (erro => console.log ("Erro: " + erro))
 
-    fatorial (-1)
-    .then (res => console.log ("Res: " + res))
-    .catch (erro => console.log("Erro: " + erro))
-}
+//     fatorial (-1)
+//     .then (res => console.log ("Res: " + res))
+//     .catch (erro => console.log("Erro: " + erro))
+// }
 
-// chamaUsandoThenCatch()
+// // chamaUsandoThenCatch()
 
 
-async function chamaUsandoAwait (){
-    try{
-        const f1 = await fatorial(5)
-        console.log ("f1: " + f1)
-        const f2 = await fatorial (-1)
-        console.log ("f2: " + f2)
-    }catch (e){
-        console.log ("e: " + e)
-    }
-}
+// async function chamaUsandoAwait (){
+//     try{
+//         const f1 = await fatorial(5)
+//         console.log ("f1: " + f1)
+//         const f2 = await fatorial (-1)
+//         console.log ("f2: " + f2)
+//     }catch (e){
+//         console.log ("e: " + e)
+//     }
+// }
 
-chamaUsandoAwait()
+// chamaUsandoAwait()
 
 
 
@@ -74,33 +75,80 @@ chamaUsandoAwait()
 
 // // console.log(url)
 
-axios.get(url)
-    .then(res => {
-        //console.log(res)
-        return res.data
-    })
-    .then(x => {
-        //console.log("Quantidade: " + x.cnt)
-        return x['list']
-    })
-    .then(res => {
-        //console.log ("A lista: " + res)
-        return res
-    })
-    .then(lista => {
+async function test() {
+    const appid = 'ef0b0973b783e0614ac87612ec04344b'
+
+    const q = 'Itu'
+
+    const units = 'metric'
+
+    const lang = "pt_br"
+
+    const cnt = 10
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`
+    try {
+        const response = await axios.get(url)
+        const data = response.data
+        const lista = data.list
         for (let previsao of lista) {
-            //console.log("Previsao da vez: " + JSON.stringify(previsao))
-            console.log(`
-            ${new Date(+previsao.dt * 1000).toLocaleString()},
-            Min: ${previsao['main']['temp_min']}\u00B0C,
-            Max: ${previsao['main']['temp_max']}\u00B0C,
-            Hum: ${previsao['main']['humidity']}%,
-            ${previsao['weather'][0]['description']}
-        `
-            )
+            console.log(` ${new Date(+previsao.dt * 1000).toLocaleString()}, Min: ${previsao['main']['temp_min']}\u00B0C, Max: ${previsao['main']['temp_max']}\u00B0C, Hum: ${previsao['main']['humidity']}%, ${previsao['weather'][0]['description']} `)
         }
-        return lista
-    })
+        const aux = lista.filter(previsao => previsao['weather'][0]['description'].startsWith('c'))
+        console.log(aux.length)
+    } catch (error) { console.log(`Error: ${error}`) }
+}
+test()
+
+// async function ex (){
+
+
+// const appid = 'ef0b0973b783e0614ac87612ec04344b'
+
+// const q = 'Itu'
+
+// const units = 'metric'
+
+// const lang = "pt_br"
+
+// const cnt = 10
+
+// const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`
+
+// const data = await axios.get(url)
+// console.log (data)
+
+// }
+
+// ex()
+
+// axios.get(url)
+//     .then(res => {
+//         //console.log(res)
+//         return res.data
+//     })
+//     .then(x => {
+//         //console.log("Quantidade: " + x.cnt)
+//         return x['list']
+//     })
+//     .then(res => {
+//         //console.log ("A lista: " + res)
+//         return res
+//     })
+//     .then(lista => {
+//         for (let previsao of lista) {
+//             //console.log("Previsao da vez: " + JSON.stringify(previsao))
+//             console.log(`
+//             ${new Date(+previsao.dt * 1000).toLocaleString()},
+//             Min: ${previsao['main']['temp_min']}\u00B0C,
+//             Max: ${previsao['main']['temp_max']}\u00B0C,
+//             Hum: ${previsao['main']['humidity']}%,
+//             ${previsao['weather'][0]['description']}
+//         `
+//             )
+//         }
+//         return lista
+//     })
 //     // .then(lista => { let count = 0; for (previsao of lista) { if (String(previsao['weather'][0]['description']).startsWith('c')) { count += 1; } } console.log(count) })
 //     // .then(lista => { let aux = 0; for (let i of lista) { if (i['weather'][0]['description'].substr(0, 1) === 'c') { aux += 1 } } console.log(aux) })
 //     .then (lista => {
